c8c2865ad04ec7d2a4364c909592e5c4d811e5ae
Alexey <Domoveek@gmail.com>
2019-10-23 22:05:45 +0300

    Increased application functionality. New icons added. The functions of calling the calculator, explorer, notepad are described. Renamed some old features

--- NavigatorRV/MainWindow.xaml.cs
+++ NavigatorRV/MainWindow.xaml.cs
@@ -954,6 +954,80 @@ Enter - open
 
 
 
+        private void FtpConnect_Click(object sender, RoutedEventArgs e)
+        {
+            FtpConnectWindow ftpConnect = new FtpConnectWindow();
+            ftpConnect.Show();
+        }
+
+        private void ZipFile_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenNotepad_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("notepad");
+        }
+
+        private void OpenCalculator_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("calc");
+        }
+
+        private void ResetExplorer_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("explorer");
+        }
+
+        //static void Main(string[] args)
+        //{
+        //    string sourceFile = "D://test/book.pdf"; // исходный файл
+        //    string compressedFile = "D://test/book.gz"; // сжатый файл
+        //    string targetFile = "D://test/book_new.pdf"; // восстановленный файл
+
+        //    // создание сжатого файла
+        //    Compress(sourceFile, compressedFile);
+        //    // чтение из сжатого файла
+        //    Decompress(compressedFile, targetFile);
+
+        //    Console.ReadLine();
+        //}
+        //public static void Compress(string sourceFile, string compressedFile)
+        //{
+        //    // поток для чтения исходного файла
+        //    using (FileStream sourceStream = new FileStream(sourceFile, FileMode.OpenOrCreate))
+        //    {
+        //        // поток для записи сжатого файла
+        //        using (FileStream targetStream = File.Create(compressedFile))
+        //        {
+        //            // поток архивации
+        //            using (GZipStream compressionStream = new GZipStream(targetStream, CompressionMode.Compress))
+        //            {
+        //                sourceStream.CopyTo(compressionStream); // копируем байты из одного потока в другой
+        //                Console.WriteLine("Сжатие файла {0} завершено. Исходный размер: {1}  сжатый размер: {2}.",
+        //                    sourceFile, sourceStream.Length.ToString(), targetStream.Length.ToString());
+        //            }
+        //        }
+        //    }
+        //}
+        //public static void Decompress(string compressedFile, string targetFile)
+        //{
+        //    // поток для чтения из сжатого файла
+        //    using (FileStream sourceStream = new FileStream(compressedFile, FileMode.OpenOrCreate))
+        //    {
+        //        // поток для записи восстановленного файла
+        //        using (FileStream targetStream = File.Create(targetFile))
+        //        {
+        //            // поток разархивации
+        //            using (GZipStream decompressionStream = new GZipStream(sourceStream, CompressionMode.Decompress))
+        //            {
+        //                decompressionStream.CopyTo(targetStream);
+        //                Console.WriteLine("Восстановлен файл: {0}", targetFile);
+        //            }
+        //        }
+        //    }
+        //}
         private void LeftFiles_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
         {
 
